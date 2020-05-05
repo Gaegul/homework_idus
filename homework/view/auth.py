f564645c815b6a7c4dc658503251963bec1b048b
sineunju <eunjuoi0515@gmail.com>
2020-05-05 22:27:57 +0900

    [FIX] 비밀번호 포멧 검사를 위한 수정
    비밀번호 len을 비교하기 위해서 컨트롤러 단계에서 암호화하여 저장하도록 변경

--- homework/view/auth.py
+++ homework/view/auth.py
@@ -1,6 +1,5 @@
 from flask import request
 from flask_restful import Resource
-from werkzeug.security import generate_password_hash
 
 from homework.view import check_json
 from homework.controller.auth import sign_up
@@ -18,7 +17,7 @@ class Auth(Resource):
     def post(self):
 
         email = request.json['email']
-        password = generate_password_hash(request.json['password'])
+        password = request.json['password']
         phone_number = request.json['phone_number']
         name = request.json['name']
         sex = request.json['sex']
