b8aa9429103ba9f95dc582768912667f8e3f135c
sineunju <eunjuoi0515@gmail.com>
2020-05-05 22:26:53 +0900

    [ADD] blueprint 연결

--- homework/app.py
+++ homework/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 
 from homework import (JWT_SECRET_KEY, JWT_REFRESH_TOKEN_EXPIRES,
                       JWT_ACCESS_TOKEN_EXPIRES)
+from homework.router import bp_auth
 
 
 def create_app():
@@ -13,6 +14,8 @@ def create_app():
     CORS(_app, resources={
         r"/api/*": {"origin": "*"}
     })
+
+    _app.register_blueprint(bp_auth)
 
     _app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
     _app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
